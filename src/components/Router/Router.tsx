d0c0c04b3e8b1a236ec7970136a86affb2da1b68
Zach Case <github@cow.dev>
2024-05-30 14:51:26 -0600

    feat: add social links to sidebar

--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -26,8 +26,25 @@ const BaseRouteLayout = () => {
         gap={4}
         padding={4}
       >
-        <Box>
-          <LogoBadge />
+        <Box display="flex" felxGrow={0} flexDirection="column" alignItems="center" gap={2}>
+          <Box>
+            <LogoBadge />
+          </Box>
+          <Typography fontWeight="bo">
+            <Link href="https://www.linkedin.com/in/cowdotdev/" target="_blank">
+              @LinkedIn
+            </Link>
+          </Typography>
+          <Typography fontWeight="bo">
+            <Link href="https://github.com/CowDotDev" target="_blank">
+              @GitHub
+            </Link>
+          </Typography>
+          <Typography fontWeight="bo">
+            <Link href="mailto:zach@zachcase.com" target="_blank">
+              zach@zachcase.com
+            </Link>
+          </Typography>
         </Box>
         <Box>
           <Outlet />
